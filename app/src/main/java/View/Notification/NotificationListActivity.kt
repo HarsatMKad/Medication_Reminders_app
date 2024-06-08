94fd5cde67417239120f51712abdfd978ff2ed1e
Harsat <mimkal@mail.ru>
2024-06-08 08:47:57 +0700

    time fix

--- app/src/main/java/View/Notification/NotificationListActivity.kt
+++ app/src/main/java/View/Notification/NotificationListActivity.kt
@@ -133,7 +133,7 @@ class NotificationListActivity : AppCompatActivity(), NotifClickDeleteInterface,
         val notificationViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(
             NotificationViewModel::class.java)
 
-        val curentDate = LocalDateTime.now().plusHours(7)
+        val curentDate = LocalDateTime.now()
         val dtfCustom = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
         val dateString = curentDate.format(dtfCustom)
 
